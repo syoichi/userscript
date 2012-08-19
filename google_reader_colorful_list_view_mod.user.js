ca18679671b5beb6f14d6c9d8fc1beedc1c55872
syoichi <syoichi@outlook.com>
2012-08-19 18:53:19 +0900

    "Google Reader Colorful List View Mod": v0.0.2
    -introduce nodeObserver
    -minor fix coding style

--- google_reader_colorful_list_view_mod.user.js
+++ google_reader_colorful_list_view_mod.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Google Reader Colorful List View Mod
 // @namespace      https://github.com/syoichi/userscript
-// @version        0.0.1
+// @version        0.0.2
 // @description    colorize item headers in Google Reader List view.
 // @include        http://www.google.com/reader/view/*
 // @include        https://www.google.com/reader/view/*
@@ -16,16 +16,44 @@ license: Public Domain
 confirmed:
     Windows 7 Home Premium SP1 64bit:
         Mozilla Firefox 14.0.1(Scriptish 0.1.7)
-        Google Chrome 21.0.1180.75
+        Google Chrome 21.0.1180.79
 */
 
 /*jslint browser: true, maxlen: 80*/
 // Edition 2012-08-11
 
-(function executeColorfulListView(each, win, doc) {
+(function executeColorfulListView(doc) {
     'use strict';
 
-    var entries, style, colors, setColor, eachAddedNodes, recordsReader, MO;
+    var entries, style, colors;
+
+    // via https://gist.github.com/3366491
+    function nodeObserver(callback, options) {
+        var MO, hasOnly, each, nodesType;
+
+        MO = window.MutationObserver || window.WebKitMutationObserver;
+        hasOnly = options.addOnly || options.removeOnly;
+        each = Array.prototype.forEach;
+        nodesType = (options.addOnly ? 'add' : 'remov') + 'edNodes';
+        options.callback = callback;
+
+        function eachNodes(mutation) {
+            function callWithInfo(node) {
+                callback({node: node, mutation: mutation, options: options});
+            }
+
+            if (mutation.type === 'childList' && hasOnly) {
+                each.call(mutation[nodesType], callWithInfo);
+            } else {
+                callWithInfo(null);
+            }
+        }
+
+        (options.observer = new MO(function eachMutations(mutations) {
+            mutations.forEach(eachNodes);
+        })).observe(options.target, options);
+        return options;
+    }
 
     entries = doc.getElementById('entries');
 
@@ -46,23 +74,24 @@ confirmed:
         ''
     ].join('\n');
 
-    setColor = function setColor(target) {
-        var targetClassList, title, titleLength, hue;
+    nodeObserver(function setColor(info) {
+        var node, nodeClassList, title, titleLength, hue;
 
-        targetClassList = target.classList;
+        node = info.node;
+        nodeClassList = node.classList;
 
-        if (!targetClassList || !targetClassList.contains('entry')) {
+        if (!(nodeClassList && nodeClassList.contains('entry'))) {
             return;
         }
 
-        title = target.querySelector('.entry-source-title');
+        title = node.querySelector('.entry-source-title');
 
         if (!title) {
             return;
         }
 
         title = title.textContent.replace(/\W/g, '-');
-        target.setAttribute('colored', title);
+        node.setAttribute('colored', title);
 
         if (colors[title] !== void 0) {
             return;
@@ -97,21 +126,9 @@ confirmed:
             '}',
             ''
         ].join('\n');
-    };
-
-    eachAddedNodes = function eachAddedNodes(record) {
-        each.call(record.addedNodes, setColor);
-    };
-    recordsReader = function recordsReader(callback) {
-        return function eachRecords(records) {
-            records.forEach(callback);
-        };
-    };
-
-    MO = win.MutationObserver || win.WebKitMutationObserver;
-
-    new MO(recordsReader(eachAddedNodes)).observe(
-        entries,
-        {childList: true}
-    );
-}(Array.prototype.forEach, window, document));
+    }, {
+        target: entries,
+        addOnly: true,
+        childList: true
+    });
+}(document));
