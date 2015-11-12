9d9d28130edde4f6127a81367cc185ba3ddf381a
Syoichi Tsuyuhara <syoichi@outlook.com>
2015-11-12 17:57:13 +0900

    "Gyazo Image Redirector": v0.0.2
    * Follow Gyazo Changes

--- src/gyazo_image_redirector.user.js
+++ src/gyazo_image_redirector.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name           Gyazo Image Redirector
 // @namespace      https://github.com/syoichi/userscript
-// @version        0.0.1
+// @version        0.0.2
 // @description    redirect Gyazo image.
-// @include        http://gyazo.com/*
+// @include        https://gyazo.com/*
 // @run-at         document-end
+// @grant          none
 // ==/UserScript==
 
 /* User Script info
-license: Public Domain
+license: CC0 1.0 Universal
 confirmed:
   Windows 7 Home Premium SP1 64bit:
-    Mozilla Firefox 27.0.1(Scriptish 0.1.11)
+    Mozilla Firefox 42.0(Greasemonkey 3.5)
+    Google Chrome 46.0.2490.86
 */
 
 // via http://let.hatelabo.jp/taizooo/let/hLHUr72VjPlW
@@ -19,9 +21,15 @@ confirmed:
 (function executeGyazoImageRedirector(doc, lc) {
   'use strict';
 
-  var gyazoImg = doc.getElementById('gyazo_img');
-
-  if (gyazoImg && /^\/[0-9a-f]{32}$/.test(lc.pathname)) {
-    lc.href = gyazoImg.src;
+  if (!/^\/[0-9a-f]{32}$/.test(lc.pathname)) {
+    return;
   }
+
+  let image = doc.querySelector('.image');
+
+  if (!image) {
+    return;
+  }
+
+  lc.href = image.src;
 }(document, location));
