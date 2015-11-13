efce17f239cf7f461f63470d9c7bd17075d9a60e
Syoichi Tsuyuhara <syoichi@outlook.com>
2015-11-13 20:06:58 +0900

    "Feedly Cloud Colorful List View": v0.0.3
    * Update nodeObserver()

--- src/feedly_cloud_colorful_list_view.user.js
+++ src/feedly_cloud_colorful_list_view.user.js
@@ -1,39 +1,37 @@
 // ==UserScript==
 // @name           Feedly Cloud Colorful List View
 // @namespace      https://github.com/syoichi/userscript
-// @version        0.0.2
+// @version        0.0.3
 // @description    colorize item headers in Feedly Cloud Title Only View.
 // @include        http://feedly.com/*
 // @include        https://feedly.com/*
 // @run-at         document-end
+// @grant          none
 // ==/UserScript==
 
 /* User Script info
-license: Public Domain
+license: CC0 1.0 Universal
 confirmed:
-  Feedly Cloud 17.12:
-    Windows 7 Home Premium SP1 64bit:
-      Mozilla Firefox 27.0.1(Scriptish 0.1.11)
+  Windows 7 Home Premium SP1 64bit:
+    Mozilla Firefox 42.0(Greasemonkey 3.5)
+    Google Chrome 46.0.2490.86
 */
 
 (function executeColorfulListView(doc, forEach) {
   'use strict';
 
-  var body = doc.body,
-    style = doc.head.appendChild(doc.createElement('style')),
-    sheet = style.sheet,
-    cssRules = sheet.cssRules,
-    colors = {},
-    feedlyPageOptions,
-    sectionOptions;
+  let body = doc.body;
+  let sheet = doc.head.appendChild(doc.createElement('style')).sheet;
+  let cssRules = sheet.cssRules;
+  let colors = {};
+  let feedlyPageOptions;
+  let sectionOptions;
 
   // via https://gist.github.com/syoichi/3366491
   function nodeObserver(callback, options) {
-    var hasOnly, each, nodesType, observer;
-
-    hasOnly = options.addOnly || options.removeOnly;
-    each = Array.prototype.forEach;
-    nodesType = options.addOnly ? 'addedNodes' : 'removedNodes';
+    let hasOnly = options.addOnly || options.removeOnly;
+    let each = Array.prototype.forEach;
+    let nodesType = options.addOnly ? 'addedNodes' : 'removedNodes';
 
     function eachNodes(record) {
       function callWithInfo(node) {
@@ -51,120 +49,115 @@ confirmed:
       }
     }
 
-    function eachMutations(mutations) {
+    let observer = new MutationObserver(mutations => {
       mutations.forEach(eachNodes);
-    }
+    });
 
-    observer = new window.MutationObserver(eachMutations);
     observer.observe(options.target, options);
 
-    options.observer = observer;
-    options.callback = callback;
-
-    return options;
+    return Object.assign(options, {
+      observer: observer,
+      callback: callback
+    });
   }
 
-  function getHueFromFeedTitle(feedTitle) {
-    var idx = 0,
-      len = feedTitle.length,
-      hue = 0;
-
-    for (; idx < len; idx += 1) {
-      hue += feedTitle[idx].charCodeAt();
-    }
-
-    return hue % 360;
-  }
   function setColor(info) {
-    var node, nodeClassList, feedID, title, hue;
+    let node = info.node;
 
-    node = info.node;
-    nodeClassList = node.classList;
-
-    if (!(nodeClassList && nodeClassList.contains('u0Entry'))) {
+    if (!(
+      node.nodeType === Node.ELEMENT_NODE && node.classList.contains('u0Entry')
+    )) {
       return;
     }
 
-    feedID = node.dataset.feedId = node.id.split(':')[0];
+    let feedID = node.dataset.feedId = node.id.split(':')[0];
 
     if (typeof colors[feedID] !== 'undefined') {
       return;
     }
 
-    title = node.querySelector('.sourceTitle > a') ||
+    let title = node.querySelector('.sourceTitle > a') ||
       doc.querySelector('#floatingTitleBar > a');
 
     if (!title) {
       return;
     }
 
-    hue = colors[feedID] = getHueFromFeedTitle(
-      title.textContent.trim().replace(/\W/g, '-')
+    let str = title.textContent.trim().replace(/\W/g, '-');
+    let hue = colors[feedID] = [...str].reduce(
+      (num, char) => num + char.charCodeAt(),
+      0
     );
 
     sheet.insertRule([
-      'body.stretched #section0_column0 > [id^="' + feedID + '"] {',
-      '  background-color: hsl(' + hue + ', 70%, 80%) !important;',
+      `body.stretched #section0_column0 > [id^="${feedID}"] {`,
+      `  background-color: hsl(${hue}, 70%, 80%) !important;`,
       '}'
     ].join(''), cssRules.length);
     sheet.insertRule([
-      'body.stretched #section0_column0 > [id^="' + feedID + '"]:hover {',
-      '  background-color: hsl(' + hue + ', 90%, 85%) !important;',
+      `body.stretched #section0_column0 > [id^="${feedID}"]:hover {`,
+      `  background-color: hsl(${hue}, 90%, 85%) !important;`,
       '}'
     ].join(''), cssRules.length);
     sheet.insertRule([
-      'body.stretched #section0_column0 > [id^="' + feedID + '"]' +
+      `body.stretched #section0_column0 > [id^="${feedID}"]` +
         '[style*="opacity"] {',
-      '  background-color: hsl(' + hue + ', 50%, 90%) !important;',
+      `  background-color: hsl(${hue}, 50%, 90%) !important;`,
       '}'
     ].join(''), cssRules.length);
     sheet.insertRule([
-      'body.stretched #section0_column0 > [id^="' + feedID + '"]' +
+      `body.stretched #section0_column0 > [id^="${feedID}"]` +
         '[style*="opacity"]:hover {',
-      '  background-color: hsl(' + hue + ', 70%, 95%) !important;',
+      `  background-color: hsl(${hue}, 70%, 95%) !important;`,
       '}'
     ].join(''), cssRules.length);
   }
+
   function wrap(entry) {
     setColor({node: entry});
   }
 
-  nodeObserver(function getFeedlyPage(bodyInfo) {
-    var feedlyCenter = bodyInfo.node;
+  function getEntries(info) {
+    let node = info.node;
+
+    if (node.id !== 'section0_column0') {
+      return;
+    }
+
+    forEach.call(node.children, wrap);
+
+    if (sectionOptions) {
+      sectionOptions.observer.disconnect();
+    }
+
+    sectionOptions = nodeObserver(setColor, {
+      target: node,
+      addOnly: true,
+      childList: true
+    });
+  }
+
+  nodeObserver(info => {
+    let feedlyCenter = info.node;
 
     if (
-      feedlyCenter.id === 'feedlyCenter' && (
-        !body.classList.contains('notlogged') ||
-          body.getAttribute('_pageid') === 'null'
-      )
+      feedlyCenter.id !== 'feedlyCenter' ||
+        body.classList.contains('notlogged') &&
+        body.getAttribute('_pageid') !== 'null'
     ) {
-      if (feedlyPageOptions) {
-        feedlyPageOptions.observer.disconnect();
-      }
-
-      feedlyPageOptions = nodeObserver(function getEntries(feedlyPageInfo) {
-        var node = feedlyPageInfo.node;
-
-        if (node.id === 'section0_column0') {
-          forEach.call(node.children, wrap);
-
-          if (sectionOptions) {
-            sectionOptions.observer.disconnect();
-          }
-
-          sectionOptions = nodeObserver(setColor, {
-            target: node,
-            addOnly: true,
-            childList: true
-          });
-        }
-      }, {
-        target: doc.getElementById('feedlyPage'),
-        addOnly: true,
-        childList: true,
-        subtree: true
-      });
+      return;
     }
+
+    if (feedlyPageOptions) {
+      feedlyPageOptions.observer.disconnect();
+    }
+
+    feedlyPageOptions = nodeObserver(getEntries, {
+      target: doc.getElementById('feedlyPage'),
+      addOnly: true,
+      childList: true,
+      subtree: true
+    });
   }, {
     target: body,
     addOnly: true,
