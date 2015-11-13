2964ed1ead4ff01cbb972ba5cfc62db577f419f9
Syoichi Tsuyuhara <syoichi@outlook.com>
2015-11-13 18:33:03 +0900

    "Hatena Bookmark Counter for Feedly Cloud": v0.0.2
    * Update nodeObserver()

--- src/hatena_bookmark_counter_for_feedly_cloud.user.js
+++ src/hatena_bookmark_counter_for_feedly_cloud.user.js
@@ -1,35 +1,34 @@
 // ==UserScript==
 // @name           Hatena Bookmark Counter for Feedly Cloud
 // @namespace      https://github.com/syoichi/userscript
-// @version        0.0.1
+// @version        0.0.2
 // @description    show Hatena Bookmark Counter Image of Hatena Bookmark Hot Entry Items in Feedly Cloud Title Only View.
 // @include        http://feedly.com/*
 // @include        https://feedly.com/*
 // @run-at         document-end
+// @grant          none
 // ==/UserScript==
 
 /* User Script info
-license: Public Domain
+license: CC0 1.0 Universal
 confirmed:
-  Feedly Cloud 17.12:
-    Windows 7 Home Premium SP1 64bit:
-      Mozilla Firefox 27.0.1(Scriptish 0.1.11)
+  Windows 7 Home Premium SP1 64bit:
+    Mozilla Firefox 42.0(Greasemonkey 3.5)
+    Google Chrome 46.0.2490.86
 */
 
 (function executeShowHatenaBookmarkCounter(doc) {
   'use strict';
 
-  var body, feedlyPageOptions, sectionOptions;
-
-  body = doc.body;
+  let body = doc.body;
+  let feedlyPageOptions;
+  let sectionOptions;
 
   // via https://gist.github.com/syoichi/3366491
   function nodeObserver(callback, options) {
-    var hasOnly, each, nodesType, observer;
-
-    hasOnly = options.addOnly || options.removeOnly;
-    each = Array.prototype.forEach;
-    nodesType = options.addOnly ? 'addedNodes' : 'removedNodes';
+    let hasOnly = options.addOnly || options.removeOnly;
+    let each = Array.prototype.forEach;
+    let nodesType = options.addOnly ? 'addedNodes' : 'removedNodes';
 
     function eachNodes(record) {
       function callWithInfo(node) {
@@ -47,62 +46,68 @@ confirmed:
       }
     }
 
-    function eachMutations(mutations) {
+    let observer = new MutationObserver(mutations => {
       mutations.forEach(eachNodes);
-    }
+    });
 
-    observer = new window.MutationObserver(eachMutations);
     observer.observe(options.target, options);
 
-    options.observer = observer;
-    options.callback = callback;
-
-    return options;
+    return Object.assign(options, {
+      observer: observer,
+      callback: callback
+    });
   }
 
   function addCounter(info) {
-    var node = info.node;
+    let node = info.node;
 
-    if (/^http:\/\/b\.hatena\.ne\.jp\/entry\/image\//.test(node.src)) {
-      info.record.target.appendChild(node);
+    if (!/^http:\/\/b\.hatena\.ne\.jp\/entry\/image\//.test(node.src)) {
+      return;
     }
+
+    info.record.target.appendChild(node);
   }
 
-  nodeObserver(function getFeedlyPage(bodyInfo) {
-    var feedlyCenter = bodyInfo.node;
+  function getEntries(info) {
+    let node = info.node;
+
+    if (node.id !== 'section0_column0') {
+      return;
+    }
+
+    if (sectionOptions) {
+      sectionOptions.observer.disconnect();
+    }
+
+    sectionOptions = nodeObserver(addCounter, {
+      target: node,
+      removeOnly: true,
+      childList: true,
+      subtree: true
+    });
+  }
+
+  nodeObserver(info => {
+    let feedlyCenter = info.node;
 
     if (
-      feedlyCenter.id === 'feedlyCenter' && (
-        !body.classList.contains('notlogged') ||
-          body.getAttribute('_pageid') === 'null'
-      )
+      feedlyCenter.id !== 'feedlyCenter' ||
+        body.classList.contains('notlogged') &&
+        body.getAttribute('_pageid') !== 'null'
     ) {
-      if (feedlyPageOptions) {
-        feedlyPageOptions.observer.disconnect();
-      }
-
-      feedlyPageOptions = nodeObserver(function getEntries(feedlyPageInfo) {
-        var node = feedlyPageInfo.node;
-
-        if (node.id === 'section0_column0') {
-          if (sectionOptions) {
-            sectionOptions.observer.disconnect();
-          }
-
-          sectionOptions = nodeObserver(addCounter, {
-            target: node,
-            removeOnly: true,
-            childList: true,
-            subtree: true
-          });
-        }
-      }, {
-        target: doc.getElementById('feedlyPage'),
-        addOnly: true,
-        childList: true,
-        subtree: true
-      });
+      return;
     }
+
+    if (feedlyPageOptions) {
+      feedlyPageOptions.observer.disconnect();
+    }
+
+    feedlyPageOptions = nodeObserver(getEntries, {
+      target: doc.getElementById('feedlyPage'),
+      addOnly: true,
+      childList: true,
+      subtree: true
+    });
   }, {
     target: body,
     addOnly: true,
